e79b4b991eea9b2bab82f07c53bc970bb79bfe16
Adrià Marcet <adriamarcetrovira@gmail.com>
2021-12-30 20:16:32 +0100

    Add initial webpack cofiguration

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
+};
